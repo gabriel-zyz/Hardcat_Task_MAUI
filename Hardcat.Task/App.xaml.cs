aa0cef238f1849daf37c0d6b63794a1a51c7ee50
Yuzhou Zhang <yzha0945@student.monash.edu>
2025-02-24 20:47:07 +1100

    Add project files.

---
+++ Hardcat.Task/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Hardcat.Task
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Hardcat.Task/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hardcat.Task
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
